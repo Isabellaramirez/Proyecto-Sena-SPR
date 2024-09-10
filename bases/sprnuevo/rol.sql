0e7d53429b1a7890e55e93e8c58d275e0d17c959
isabella <kateryne_ramirez@soy.sena.edu.co>
2024-09-10 12:49:12 -0500

    carpeta bses eliminada

--- bases/sprnuevo/rol.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM spr.rol;
-SELECT * FROM spr.rol;
-insert into spr.rol (id_rol, nombre_rol) values (1, 'prestador de servicios');
-insert into spr.rol (id_rol, nombre_rol) values (2, 'contratista');

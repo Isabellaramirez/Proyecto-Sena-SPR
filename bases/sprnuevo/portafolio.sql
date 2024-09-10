7dc5d3c15d37540ca02adb480091e98715d85d50
isabellaramirez <kateryne_ramirez@soy.sena.edu.co>
2024-09-10 12:23:58 -0500

    bases de datos

---
+++ bases/sprnuevo/portafolio.sql
@@ -0,0 +1,8 @@
+SELECT * FROM spr.portafolio;
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (20, 'descripcion portafolio 1', 'imagenpor1.jpg', 1234567890);
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (23, 'descripcion portafolio 4', 'imagenpor4.jpg', 1234567893);
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (25, 'descripcion portafolio 6', 'imagenpor6.jpg', 1234567895);
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (28, 'descripcion portafolio 9', 'imagenpor9.jpg', 1234567898);
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (30, 'descripcion portafolio 11', 'imagenpor11.jpg', 123678910);
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (32, 'descripcion portafolio 13', 'imagenpor13.jpg', 123678912);
+insert into spr.portafolio (id_portafolio, descripcion, imagenes, usuario_cedula) values (34, 'descripcion portafolio 15', 'imagenpor15.jpg', 123678914);
---
+++ bases/sprnuevo/usuario.sql
@@ -0,0 +1,32 @@
+SELECT * FROM spr.usuario;
+
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567890', 'Juan', 'Pérez', '0987654321', 'Calle 123', 'Descripción 1', 'juan.perez@example.com', '1990-01-01', 'foto1.jpg', 'pass123', 1);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567891', 'María', 'Gómez', '0987654322', 'Calle 124', 'Descripción 2', 'maria.gomez@example.com', '1991-02-02', 'foto2.jpg', 'pass124', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567892', 'Carlos', 'López', '0987654323', 'Calle 125', 'Descripción 3', 'carlos.lopez@example.com', '1992-03-03', 'foto3.jpg', 'pass125', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567893', 'Ana', 'Martínez', '0987654324', 'Calle 126', 'Descripción 4', 'ana.martinez@example.com', '1993-04-04', 'foto4.jpg', 'pass126', 1);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567894', 'Luis', 'Hernández', '0987654325', 'Calle 127', 'Descripción 5', 'luis.hernandez@example.com', '1994-05-05', 'foto5.jpg', 'pass127', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567895', 'Sofía', 'García', '0987654326', 'Calle 128', 'Descripción 6', 'sofia.garcia@example.com', '1995-06-06', 'foto6.jpg', 'pass128', 1);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567896', 'Miguel', 'Rodríguez', '0987654327', 'Calle 129', 'Descripción 7', 'miguel.rodriguez@example.com', '1996-07-07', 'foto7.jpg', 'pass129', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567897', 'Laura', 'Fernández', '0987654328', 'Calle 130', 'Descripción 8', 'laura.fernandez@example.com', '1997-08-08', 'foto8.jpg', 'pass130', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567898', 'Pedro', 'Sánchez', '0987654329', 'Calle 131', 'Descripción 9', 'pedro.sanchez@example.com', '1998-09-09', 'foto9.jpg', 'pass131', 1);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('1234567899', 'Elena', 'Ramírez', '0987654330', 'Calle 132', 'Descripción 10', 'elena.ramirez@example.com', '1999-10-10', 'foto10.jpg', 'pass132', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('123678910', 'Jorge', 'Torres', '0987654331', 'Calle 133', 'Descripción 11', 'jorge.torres@example.com', '1988-11-11', 'foto11.jpg', 'pass133', 1);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('123678911', 'Gabriela', 'Flores', '0987654332', 'Calle 134', 'Descripción 12', 'gabriela.flores@example.com', '1989-12-12', 'foto12.jpg', 'pass134', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('123678912', 'Manuel', 'Morales', '0987654333', 'Calle 135', 'Descripción 13', 'manuel.morales@example.com', '1987-01-13', 'foto13.jpg', 'pass135', 1);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('123678913', 'Patricia', 'Castro', '0987654334', 'Calle 136', 'Descripción 14', 'patricia.castro@example.com', '1986-02-14', 'foto14.jpg', 'pass136', 2);
+INSERT INTO spr.usuario (cedula, nombre, apellido, telefono, direccion, descripcion, correo, fecha_nacimiento, foto, contraseña, rol_id_rol) VALUES
+('123678914', 'Alberto', 'Ortiz', '0987654335', 'Calle 137', 'Descripción 15', 'alberto.ortiz@example.com', '1985-03-15', 'foto15.jpg', 'pass137', 1);
